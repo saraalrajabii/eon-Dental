e5948731b560f3f6a833393c9e7dfa409f3d9035
saraalrajabii <saraalrajabii12@gmail.com>
2021-03-03 17:38:49 +0200

    set-state-for-first-image

--- src/components/GetStarted.js
+++ src/components/GetStarted.js
@@ -7,7 +7,10 @@ import bite_issue from "./funnel3/funnel3/bite_issue.png";
 import crossbite from "./funnel3/funnel3/crossbite.png";
 // for styling this connect with index.css
   function GetStarted () {
-    const [active, setActive] = useState(""); 
+    const [state, setState] = useState(""); 
+
+   // state changed so it will print the new value 
+    console.log(state);
 
     return (
       <div  className="container">
@@ -24,13 +27,14 @@ import crossbite from "./funnel3/funnel3/crossbite.png";
           <div className="title-question"><p className="element2">What’s your biggest concern with your smile?</p></div>
         </div>
         <div className="grid-session">
-        <div className="grid-item"> <img src={crowded} className="teeth"/><p>crowded</p></div>
-        <div className="grid-item"> <img src={spacing} className="teeth"/><p>spacing</p></div>
-        <div className="grid-item"> <img src={crossbite} className="teeth"/><p>crossbite</p></div>
-        <div className="grid-item"> <img src={bite_issue} className="teeth"/><p>bite_issue</p></div>
+        <div className="grid-item" onClick={() => setState("crowded")}> <img src={crowded} className="teeth"/><p>crowded</p></div>
+        <div className="grid-item" onClick={() => setState("spacing")}> <img src={spacing} className="teeth"/><p>spacing</p></div>
+        <div className="grid-item" onClick={() => setState("crossbite")}> <img src={crossbite} className="teeth"/><p>crossbite</p></div>
+        <div className="grid-item" onClick={() => setState("bite_issue")}> <img src={bite_issue} className="teeth"/><p>bite_issue</p></div>
         <div className="grid-item"><p className="note">I have another issue</p></div>
         </div>
-      
+       
+      {/* // here will appere when i click on some things */}
         <div className="question2-container">
           <div className="circle"><p className="circle-element">2</p></div>
           <div className="title-question"><p className="element2">Choose the image that best describes your spaced teeth:</p></div>
